c3079e55dea7ae4e4e1e2ee21d0de0f49e263937
sahasuvo <36543362+sahasuvo@users.noreply.github.com>
2018-02-16 22:23:23 +0530

    czcdfdsfsds

---
+++ MVCWidoutEF/MVCWidoutEF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWidoutEF.MvcApplication" Language="C#" %>
---
+++ MVCWidoutEF/MVCWidoutEF/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCWidoutEF.Models
+{
+    public class StudentModel
+    {
+        [Display (AutoGenerateField =true, Name="Id")]
+        public int ID { get; set; }
+        [Required (AllowEmptyStrings =false,ErrorMessage = "Please Enter Name")]
+        public string Name { get; set; }
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please Enter City Name")]
+        public string CityName { get; set; }
+    }
+}
